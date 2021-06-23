938ac1763b0aa07ef7f5af976530a37eab38e2a8
randomguy70 <onlychessfreeze0515@gmail.com>
2021-06-23 14:51:03 -0500

    wqertyufgdghmfdtghjk,fgnhjykh

--- smart_notes_ce/src/main.h
+++ smart_notes_ce/src/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-// standard includes
+// standard includes. i don't want to have to type them out at the beginning of every last .c file, so i just put include main.h instead. I don't care what MateoC says if he comes with some obscure rule about includes that says i am in error :P....
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -23,16 +23,17 @@
 #include "includes/file.h"
 
 // defines
-#define BLACK        0  // defines the fixed-entry offset of black in my custom palette
-#define WHITE        1  // defines the fixed-entry offset of white in my custom palette
-#define TRANSPARENT  2  // defines the fixed-entry offset of transparent color in my custom palette
-#define DARK_GREY    3  // defines the fixed-entry offset of dark grey in my custom palette
-#define MEDIUM_GREY  4  // defines the fixed-entry offset of medium grey in my custom palette
-#define LIGHT_GREY   5  // defines the fixed-entry offset of light grey in my custom palette
-#define DARK_BLUE    6  // defines the fixed-entry offset of dark blue in my custom palette
-#define RED          7  // defines the fixed-entry offset of red in my custom palette
+#define BLACK         0  // defines the fixed-entry offset of black in my custom palette
+#define WHITE         1  // defines the fixed-entry offset of white in my custom palette
+#define TRANSPARENT   2  // defines the fixed-entry offset of transparent color in my custom palette
+#define DARK_GREY     3  // defines the fixed-entry offset of dark grey in my custom palette
+#define MEDIUM_GREY   4  // defines the fixed-entry offset of medium grey in my custom palette
+#define LIGHT_GREY    5  // defines the fixed-entry offset of light grey in my custom palette
+#define DARK_BLUE     6  // defines the fixed-entry offset of dark blue in my custom palette
+#define RED           7  // defines the fixed-entry offset of red in my custom palette
+#define LIGHTER_BLUE  8  // defines the fixed-entry offset of blue in my custom palette
 
-/*necessary information
+/* necessary but random info
 -3 modes
    mode 1 = homescreen
    mode 2 = editor
@@ -41,7 +42,6 @@
    txtMode 1 = math
    txtMode 2 = caps
    txtMode 3 = lowercase
-
 */
 
 #endif
